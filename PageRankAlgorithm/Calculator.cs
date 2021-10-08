263c84d4925ca1a11b5939a7753c3c7b7081b980
DuckOnDope805 <92157017+DuckOnDope805@users.noreply.github.com>
2021-10-08 12:58:48 +0200

    Add files via upload

---
+++ PageRankAlgorithm/Calculator.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PageRankAlgorithm
+{
+    class Calculator
+    {
+        List<PageNode> PageNodes = null;
+        SQLiteDB Database = null;
+        string commentchar = "#######################";
+
+        public Calculator()
+        {
+            Initialize();
+        }
+        public bool Initialize()
+        {
+            PageNodes = new List<PageNode>();
+            return true;
+        }
+
+        public void Calculate()
+        {
+
+            Console.Write(commentchar + " " + DateTime.Now.ToString("dd.MM.yyyy HH:mm:ss:fff") + " " + commentchar);
+            Console.WriteLine();
+
+            Database = new SQLiteDB();
+
+            List<User> users = Get_EntrysUser();
+            List<PageLinks> links = Get_EntrysPageLink();
+            List<Page> pages = Get_EntrysPages();
+
+            InitializePageNodes(pages);
+
+            GetPageRank(pages, links);
+
+            Console.WriteLine();
+            Console.Write(commentchar + " " + DateTime.Now.ToString("dd.MM.yyyy HH:mm:ss:fff") + " " + commentchar);
+        }
+
+        private void GetPageRank(List<Page> pages, List<PageLinks> links)
+        {
+            foreach (Page page in pages)
+            {
+                PageNode node = PageNodes.Find(c => c.Id == page.page_id);
+                List<PageLinks> pagelinks =  links.FindAll(c => c.pl_from == node.Id);
+                foreach(PageLinks link in pagelinks)
+                {
+                    PageNode outgoing = PageNodes.Find(c => c.Name == link.pl_title);
+                    node.Output.Add(outgoing);
+                }
+                
+            }
+            foreach(Page page in pages)
+            {
+                PageNode node = PageNodes.Find(c => c.Id == page.page_id);
+                List<PageLinks> pagelinks = links.FindAll(c => c.pl_title == node.Name);
+                foreach (PageLinks link in pagelinks)
+                {
+                    PageNode input = PageNodes.Find(c => c.Id == link.pl_from);
+                    node.Input.Add(input);
+                   
+                }
+            }
+            for(int i = 0;i < 1000;)
+            {
+                foreach (PageNode node in PageNodes)
+                {
+                    Calculate_PageRank(node);
+                    
+                }
+                i++;
+            }
+            PageNodes = PageNodes.OrderByDescending(c => c.PageRank).ToList();
+            PrintScoreboard();
+        }
+
+        private void PrintScoreboard()
+        {
+            TableDrawer drawer = new TableDrawer();
+            drawer.PrintLine();
+            drawer.PrintRow("Rank", "Id", "Name", "Score");
+            drawer.PrintLine();
+
+            int rank = 1;
+            foreach (PageNode node in PageNodes)
+            {
+                drawer.PrintRow(rank.ToString(), node.Id.ToString(), node.Name.ToString(), node.PageRank.ToString());
+                rank++;
+            }
+
+            drawer.PrintLine();
+        }
+
+        private void Calculate_PageRank(PageNode node)
+        {
+            node.PageRank = (1 - (float)0.85) + (float)0.85 * CalculateTempSum(node);
+        }
+
+        private float CalculateTempSum(PageNode node)
+        {
+            float sum = 0;
+
+            foreach(PageNode input in node.Input)
+            {
+                sum = sum + (input.PageRank / input.Output.Count);
+            }
+
+            return sum;
+        }
+
+        private void InitializePageNodes(List<Page> pages)
+        {
+            foreach(Page page in pages)
+            {
+                PageNode node = new PageNode();
+                node.Id = page.page_id;
+                node.Name = page.page_title;
+                PageNodes.Add(node);
+            }
+        }
+
+        private List<User> Get_EntrysUser()
+        {
+            
+            Console.WriteLine();
+            Console.WriteLine("start searching entrys User");
+            
+
+            return Database.con.Table<User>().ToList();
+        }
+
+        private List<PageLinks> Get_EntrysPageLink()
+        {
+
+            Console.WriteLine();
+            Console.WriteLine("start searching entrys Pagelink");
+            
+
+            return Database.con.Table<PageLinks>().ToList();
+        }
+
+        private List<Page> Get_EntrysPages()
+        {
+
+            Console.WriteLine();
+            Console.WriteLine("start searching entrys Pages");
+            Console.WriteLine();
+
+
+            return Database.con.Table<Page>().ToList();
+        }
+    }
+}
+
---
+++ PageRankAlgorithm/ModelCatalog.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PageRankAlgorithm
+{
+    public class PageNode
+    {
+        public int Id { get; set; }
+        public float PageRank { get; set; } = 1;
+        public string Name { get; set; }
+        public List<PageNode> Input { get; set; } = new List<PageNode>();
+        public List<PageNode> Output { get; set; } = new List<PageNode>();
+    }
+
+    public class User
+    {
+        public int user_id { get; set; }
+        public string user_name { get; set; }
+        public string user_real_name { get; set; }
+        public string user_password { get; set; }
+        public string user_newpassword { get; set; }
+        public string user_newpass_time { get; set; }
+        public string user_email { get; set; }
+        public string user_touched { get; set; }
+        public string user_token { get; set; }
+        public string user_email_authenticated { get; set;}
+        public string user_email_token { get; set; }
+        public string user_email_token_expire { get; set; }
+        public string user_registarion { get; set; }
+        public int user_editcount { get; set; }
+        public string user_password_expires { get; set; }
+    }
+
+    public class PageLinks
+    {
+        public int pl_from { get; set; }
+        public int pl_namespace { get; set; }
+        public string pl_title { get; set; }
+        public int pl_from_namespace{ get;set;}
+    }
+
+    public class Page
+    {
+        public int page_id { get; set; }
+        public int page_namespace { get; set; }
+        public string page_title { get; set; }
+        public string page_restrictions { get; set; }
+        public int page_is_redirect { get; set; }
+        public int page_is_new { get; set; }
+        public string page_random { get; set; }
+        public string page_touched { get; set; }
+        public string page_links_updated {get;set;}
+        public int page_latest { get; set; }
+        public int page_len { get; set; }
+        public string page_content_model { get; set; }
+        public string page_lang { get; set; }
+    }
+}
